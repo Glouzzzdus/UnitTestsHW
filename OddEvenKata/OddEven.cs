eda13eb1557ed37db5628626f434546e1d2cb089
Andrii Hluzdovskyi <Andrii_Hluzdovskyi@epam.com>
2023-12-05 06:33:36 +0100

    Add IsPrime method

--- OddEvenKata/OddEven.cs
+++ OddEvenKata/OddEven.cs
@@ -6,7 +6,45 @@ using System.Threading.Tasks;
 
 namespace OddEvenKata
 {
-    internal class OddEven
+    public class OddEven
     {
+        public string GetResult(int number)
+        {
+            if (number >= 1 && number <= 100) 
+            {
+                
+            } 
+            else return string.Empty;
+
+        }
+
+        public bool IsPrime(int number)
+        {
+            
+            if (number <= 1)
+            {
+                return false;
+            }
+
+            if (number == 2)
+            {
+                return true;
+            }
+
+            if (number % 2 == 0)
+            {
+                return false;
+            }
+            
+            for (int i = 3; i <= Math.Sqrt(number); i += 2)
+            {                
+                if (number % i == 0)
+                {
+                    return false;
+                }
+            } 
+            
+            return true;
+        }
     }
 }
---
+++ OddEvenTests/OddEvenTest.cs
@@ -0,0 +1,21 @@
+using OddEvenKata;
+
+namespace OddEvenTests
+{
+    [TestClass]
+    public class OddEvenTest
+    {
+        [TestMethod]
+        public void Input1_ReturnsOdd()
+        {
+            // Arrange
+            var oddEven = new OddEven();
+
+            // Act
+            var result = oddEven.GetResult(1);
+
+            // Assert
+            Assert.AreEqual("1", result);
+        }
+    }
+}
