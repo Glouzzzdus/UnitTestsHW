18bf2b3e0b19dc90c7965f112c0f50c83bbec1ab
Andrii Hluzdovskyi <Andrii_Hluzdovskyi@epam.com>
2023-12-05 06:45:02 +0100

    Add IsComposite method

--- OddEvenKata/OddEven.cs
+++ OddEvenKata/OddEven.cs
@@ -21,30 +21,26 @@ namespace OddEvenKata
         public bool IsPrime(int number)
         {
             
-            if (number <= 1)
-            {
-                return false;
-            }
+            if (number <= 1) return false;
 
-            if (number == 2)
-            {
-                return true;
-            }
+            if (number == 2) return true;            
 
-            if (number % 2 == 0)
-            {
-                return false;
-            }
+            if (number % 2 == 0) return false;
+            
             
             for (int i = 3; i <= Math.Sqrt(number); i += 2)
             {                
-                if (number % i == 0)
-                {
-                    return false;
-                }
+                if (number % i == 0) return false;
             } 
             
             return true;
+        }
+
+        public bool IsComposite(int number)
+        {
+            if (number <= 1) return false;
+
+            return !IsPrime(number);
         }
     }
 }
