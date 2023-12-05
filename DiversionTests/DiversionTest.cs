af2656d57138736f4f3ea501c487b7c33e9824c1
Andrii Hluzdovskyi <Andrii_Hluzdovskyi@epam.com>
2023-12-05 08:57:53 +0100

    Add Diversion Test for large n

--- DiversionTests/DiversionTest.cs
+++ DiversionTests/DiversionTest.cs
@@ -34,5 +34,34 @@ namespace DiversionTests
             // Assert
             Assert.AreEqual(expected, actual);
         }
+
+        [TestMethod]
+        public void Test_CountBinarySequences_WhenNIs10()
+        {
+            // Arrange
+            int n = 10;
+            int expected = 144;
+            var sequenceCounter = new Diversion();
+
+            // Act
+            int actual = sequenceCounter.CountBinarySequencesWithoutAdjacentOnes(n);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Test_CountBinarySequences_WhenNIsLarge()
+        {
+            // Arrange
+            int n = 50;
+            var sequenceCounter = new Diversion();
+
+            // Act
+            int result = sequenceCounter.CountBinarySequencesWithoutAdjacentOnes(n);
+
+            // Assert
+            Assert.IsTrue(result > 0);
+        }
     }
 }
